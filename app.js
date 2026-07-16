6c3fddfcf962b7716d8c62b5dc9913fcc1932607
Saumyadeep Mishra <sdm.cord7@gmail.com>
2026-07-16 16:29:18 +0530

    feat: add file reading command

--- app.js
+++ app.js
@@ -1,23 +1,13 @@
 const fs = require("fs");
 
-function main() {
-    const operation = process.argv[2];
-    const filename = process.argv[3];
-
-    if (operation !== "create") {
-        console.log("Enter a valid command my G!");
-        return;
+function normalizeFilename(filename){
+    if(filename.endsWith(".txt")){
+        return filename.slice(0, -4);
     }
+        return filename;
+}
 
-    if (!filename) {
-        console.log("You don't want an empty filename!");
-        return;
-    }
-
-    const cleanFilename = filename.endsWith(".txt")
-    ? filename.slice(0, -4)
-    : filename;
-
+function create(cleanFilename){
     fs.writeFile(
         `${cleanFilename}.txt`,
         "My G just created a file",
@@ -30,6 +20,40 @@ function main() {
             console.log("File created successfully!");
         }
     );
+}
+
+function read(cleanFilename){
+    fs.readFile(`${cleanFilename}.txt`, "utf-8", (error, data)=>{
+            if(error){
+                console.log("Encountered an error", error.message);
+                return;
+            }
+            console.log("The Content: ", data);
+        })
+}
+
+function main() {
+    const operation = process.argv[2];
+    const filename = process.argv[3];
+
+    if(operation !== "create" && operation!=="read"){
+        console.log("enter a valid command my G!");
+        return;
+    }
+
+    if (!filename) {
+        console.log("You don't want an empty filename!");
+        return;
+    }
+    const cleanFilename = normalizeFilename(filename);
+
+    if (operation === "create") {
+        create(cleanFilename);
+    }
+    else if(operation === "read"){
+        read(cleanFilename);
+    }
+
 }
 
 main();
